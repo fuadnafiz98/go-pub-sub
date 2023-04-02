fb37a3aff644006f7bebf9184c5cbf78e0b319ee
fuadnafiz98 <fuadnafiz98@gmail.com>
2023-04-02 20:17:38 +0600

    basic connection done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Golang Pub-Sub With Redis
+
+## Interacting with Redis
+
+While running the redis docker container, get the redis docker container name.
+In my case, it is `go-pubsub-redis`.
+
+Now, exec into the redis container:
+
+`docker-compose exec -it go-pubsub-redis sh`
+
+Then go to redis-cli by running: `redis-cli`
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	rds := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+	ctx := context.Background()
+	val, err := rds.Get(ctx, "key").Result()
+	if err != nil {
+		fmt.Println("No value of `key`")
+	}
+	fmt.Println(val)
 }
