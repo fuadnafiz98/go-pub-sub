5cb8bcad4be3a8202b3d5f876d0f19113772085b
fuadnafiz98 <fuadnafiz98@gmail.com>
2023-04-03 19:35:19 +0600

    message between different servers are done!

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	// rds := redis.NewClient(&redis.Options{
-	// 	Addr: "localhost:6379",
-	// 	DB:   0,
-	// })
-	// ctx := context.Background()
-	// val, err := rds.Get(ctx, "key").Result()
-	// if err != nil {
-	// 	fmt.Println("No value of `key`")
-	// }
-	// fmt.Println(val)
-
 	godotenv.Load()
 
 	handler := http.NewServeMux()
 
+	// TODO: make a redis service
+	rds := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+		DB:   0,
+	})
+	ctx := context.Background()
+
 	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rds.Publish(ctx, "messages", "Message from server: "+os.Getenv("PORT"))
 		w.Write([]byte("Hello from server: " + os.Getenv("PORT")))
 	})
 
@@ -33,9 +32,26 @@ func main() {
 		Handler: handler,
 	}
 
+	// on server initialization, connect to channel `messages`
+	pubsub := rds.Subscribe(ctx, "messages")
+
+	defer pubsub.Close()
+
+	go func() {
+		for {
+			msg, err := pubsub.ReceiveMessage(ctx)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(msg.Channel, msg.Payload)
+		}
+	}()
+
 	fmt.Println("Server running on: " + "0.0.0.0:" + os.Getenv("PORT"))
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+
 }
